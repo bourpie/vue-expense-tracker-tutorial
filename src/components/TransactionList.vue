80797639bb9316e8bf56a897675d2b115555139b
Your Name <your@email.com>
2024-03-05 07:32:38 -0500

    Commit message

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,26 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li v-for="t in transactions" :class="t.amount < 0 ? 'minus' : 'plus'" :key="t.id">
+      {{ t.text }} <span>{{ t.amount }}</span>
+      
+      <button @click="deleteTransaction(t.id)" class="delete-btn" >x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+  const props = defineProps({
+    transactions: {
+      type: Array,
+      required: true
+    }
+  })
+
+  const emit = defineEmits(['transactionDeleted']);
+
+  const deleteTransaction = (id) => {
+    emit('transactionDeleted', id);
+  };
+  
+</script>
